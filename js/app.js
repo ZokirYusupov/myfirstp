8e7af0c845ac0b75e3adb5b922d9154391d733d7
Zokirbek <zyusupov51@mail.com>
2021-12-19 10:37:58 +0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,42 @@
+const navMenu = document.getElementById('nav-menu'),
+      toggleMenu = document.getElementById('nav-toggle'),
+      closeMenu = document.getElementById('nav-close');
+
+    //   show
+toggleMenu.addEventListener('click', () => {
+    navMenu.classList.toggle('show')
+})
+closeMenu.addEventListener('click', () => {
+    navMenu.classList.remove('show')
+})
+
+// remove menu
+
+const navLink = document.querySelectorAll('.nav__link');
+
+function linkAction(){
+    navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click' , linkAction));
+
+// scroll section
+const sections = document.querySelectorAll('section[id]')
+// console.log(sections);
+
+window.addEventListener('scroll', scrollActive);
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50,
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href * ='+ sectionId + ']').classList.add('active')
+        }else{
+            document.querySelector('.nav__menu a[href * ='+ sectionId + ']').classList.remove('active')
+        }
+    })
+}
